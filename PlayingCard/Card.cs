6a300d225951293e369707b8eee0c8ee623aec80
Adriana Gavrilut <adriana.gavrilut21@gmail.com>
2022-06-12 23:32:09 +0300

    Add project files.

---
+++ PlayingCard/Card.cs
@@ -0,0 +1,26 @@
+﻿namespace PlayingCard
+{
+    internal class Card
+    {
+        private int rank;
+        private int suit;
+
+        public Card()
+        {
+
+        }
+
+        public Card(int rank, int suit)
+        {
+            this.rank = rank;
+            this.suit = suit;
+        }
+
+        public int Rank { get; set; }
+
+        public int Suit { get; set; }
+
+        public int Image { get; set; }
+
+    }
+}
---
+++ PlayingCard/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayingCard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics grp;
+        Bitmap bmp;
+        Random rnd;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            grp = Graphics.FromImage(bmp);
+            rnd = new Random();
+            Card a = new Card();
+            Card b = new Card(rnd.Next(13), rnd.Next(4));
+
+            pictureBox1.Image = bmp;
+        }
+    }
+}
